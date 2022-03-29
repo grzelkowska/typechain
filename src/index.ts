6fe1a27b7e0fae4d0026079b41f59b0c7a17ecc1
Phillip <philliplee515@gmail.com>
2022-03-29 13:40:24 +0900

    creating blockchain2

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,44 @@
-import { preProcessFile } from "typescript";
+import * as CryptoJS from "crypto-js";
 
 class Block {
-    public index: number;
-    public hash: string;
-    public previousHash: string;
-    public data: string;
-    public timestamp: number;
-    constructor( 
-        index: number,
-        hash: string,
-        previousHash: string,
-        data: string,
-        timestamp: number,
-    ){
-        this.index = index;
-        this.hash = hash;
-        this.previousHash = previousHash;
-        this.data = data;
-        this.timestamp = timestamp
-    }
+  public index: number;
+  public hash: string;
+  public previousHash: string;
+  public data: string;
+  public timestamp: number;
+
+  static calculateBlockHash = (
+    index: number,
+    previousHash: string,
+    timestamp: number,
+    data: string
+  ): string =>
+    CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+
+  constructor(
+    index: number,
+    hash: string,
+    previousHash: string,
+    data: string,
+    timestamp: number
+  ) {
+    this.index = index;
+    this.hash = hash;
+    this.previousHash = previousHash;
+    this.data = data;
+    this.timestamp = timestamp;
+  }
 }
 
-const genesisBlock: Block = new Block(0, "123213213", "", "Hello", 123456)
-let blockchain: [Block] = [genesisBlock]
+const genesisBlock: Block = new Block(0, "123213213", "", "Hello", 123456);
+let blockchain: Block[] = [genesisBlock];
 
-console.log(blockchain)
+console.log(blockchain);
 
-export {}
+const getBlockchain = (): Block[] => blockchain;
+
+const getLatestBlock = () : Block => blockchain[blockchain.length - 1];
+
+const getNewTimeStamp = () : number => Math.round(new Date().getTime() / 1000)
+
+export {};
